3767c19a9fde7af4c8fe340f5cfa4354e8b9f153
La Min <laminhein2005@gmail.com>
2025-06-07 12:05:01 +0630

    add skill

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,27 +1,68 @@
 import "./About.css";
-import laminhein from "../assets/laminhein.jpg"
+import laminhein from "../assets/laminhein.jpg";
 import { useTheme } from "../context/ThemeContext";
+import { useState } from "react";
 
 let About = () => {
+  let { darkMode } = useTheme();
+  let [activeTab, setActiveTab] = useState("skill");
 
-    let {darkMode} = useTheme();
-
-    return(
-        <div className="about">
-            <div className="about_img">
-                <img src={laminhein} alt="laminhein_aboutimg" />
-            </div>
-            <div className="about_text">
-                <h1>My story</h1>
-                <p style={{color:`${darkMode ? "white" :"black"}`}} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam nihil cumque dicta, quibusd fuga. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nulla neque alias inventore quos fugiat dignissimos. Necessitatibus, distinctio tempora cumque facere velit libero excepturi voluptatem magnam exercitationem, animi, aperiam ullam?</p>
-                <div className="skill">
-                    <h4 style={{color:`${darkMode ? "white" :"black"}`}}>Skill</h4>
-                    <h4 style={{color:`${darkMode ? "white" :"black"}`}}>Experience</h4>
-                    <h4 style={{color:`${darkMode ? "white" :"black"}`}}>Education</h4>
-                </div>
-            </div>
+  return (
+    <div className="about">
+      <div className="about_img">
+        <img src={laminhein} alt="laminhein_aboutimg" />
+      </div>
+      <div className="about_text">
+        <h1>My story</h1>
+        <p style={{ color: `${darkMode ? "white" : "black"}` }}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam nihil
+          cumque dicta, quibusd fuga. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Porro nulla neque alias inventore quos fugiat
+          dignissimos. Necessitatibus, distinctio tempora cumque facere velit
+          libero excepturi voluptatem magnam exercitationem, animi, aperiam
+          ullam?
+        </p>
+        <div className="skill">
+          <h4
+            className={
+              activeTab === "skill"
+                ? "activeTab"
+                : `${darkMode ? "aboutwhite" : "aboutdark"}`
+            }
+            onClick={() => {
+              setActiveTab("skill");
+            }}
+          >
+            Skill
+          </h4>
+          <h4
+            className={
+              activeTab === "experience"
+                ? "activeTab"
+                : `${darkMode ? "aboutwhite" : "aboutdark"}`
+            }
+            onClick={() => {
+              setActiveTab("experience");
+            }}
+          >
+            Experience
+          </h4>
+          <h4
+            className={
+              activeTab === "education"
+                ? "activeTab"
+                : `${darkMode ? "aboutwhite" : "aboutdark"}`
+            }
+            onClick={() => {
+              setActiveTab("education");
+            }}
+          >
+            Education
+          </h4>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default About;
