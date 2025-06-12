8f8669766c920f27b54f664f178db22af6d9f43f
La Min <laminhein2005@gmail.com>
2025-06-12 21:13:55 +0630

    add link and other

--- src/component/Project-list.js
+++ src/component/Project-list.js
@@ -1,37 +1,41 @@
+import HA from "../assets/HA.png";
+import BookStore from  "../assets/Book Store.png";
+import Portfolio from "../assets/Portfolio.png";
+
 export let projectList = [
   {
     pid: Math.random().toString(),
-    pimg: "good",
+    pimg:HA ,
     ptilte: "HealthBridge Ayeyarwady",
     planguages: ["html", "css", "javascript"],
     pdescription: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
             minus quia ad laudantium. A ratione reprehenderit esse laboriosam
             ipsam aspernatur inventore vel neque consequatur, nostrum provident,
             impedit praesentium repellendus tempora?`,
-    plink: "link",
+    plink: "https://lamin2005.github.io/HealthBridge-Ayeyarwady/",
   },
 
   {
     pid: Math.random().toString(),
-    pimg: "good",
-    ptilte: "HealthBridge Ayeyarwady",
+    pimg:BookStore,
+    ptilte: "BookStore Shop",
     planguages: ["html", "css", "javascript"],
     pdescription: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
             minus quia ad laudantium. A ratione reprehenderit esse laboriosam
             ipsam aspernatur inventore vel neque consequatur, nostrum provident,
             impedit praesentium repellendus tempora?`,
-    plink: "link",
+    plink: "https://book-store-six-plum-84.vercel.app/",
   },
 
   {
     pid: Math.random().toString(),
-    pimg: "good",
-    ptilte: "HealthBridge Ayeyarwady",
+    pimg: Portfolio,
+    ptilte: "Portfolio",
     planguages: ["html", "css", "javascript"],
     pdescription: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
             minus quia ad laudantium. A ratione reprehenderit esse laboriosam
             ipsam aspernatur inventore vel neque consequatur, nostrum provident,
             impedit praesentium repellendus tempora?`,
-    plink: "link",
+    plink: "https://laminhein-rbs8.vercel.app/",
   },
 ];
--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -3,25 +3,55 @@ import { projectList } from "./Project-list";
 import { useTheme } from "../context/ThemeContext";
 
 let Project = () => {
-
-  let {darkMode} = useTheme();
+  let { darkMode } = useTheme();
 
   return (
     <div className="project-section">
       <h1>Projects</h1>
       <div className="project-list">
         {projectList.map((list) => {
+          console.log(list);
+
           return (
             <div className="list" key={list.pid}>
-              <div className="pimg"></div>
-              <div className="planguages">
-                <div className={`language  ${darkMode ? 'lwhite' : 'lblack'} }`} >{list.planguages[0]}</div>
-                <div className={`language  ${darkMode ? 'lwhite' : 'lblack'} }`}>{list.planguages[1]}</div>
-                <div className={`language  ${darkMode ? 'lwhite' : 'lblack'} }`}>{list.planguages[2]}</div>
+              <div className="pimg">
+                <img src={list.pimg} alt="Project_Image" />
               </div>
-              <h3 className={`pname  ${darkMode ? 'pblue' : 'pblack'} }`} style={{ color: `${darkMode ? "pwhite" : "pblack"}` }}>{list.ptilte}</h3>
-              <p className={`${darkMode ? 'pwhite' : 'pblack'} }`}>{list.pdescription}</p>
-              <button className="view_btn">View</button>
+              <div className="planguages">
+                <div
+                  className={`language  ${darkMode ? "lwhite" : "lblack"} }`}
+                >
+                  {list.planguages[0]}
+                </div>
+                <div
+                  className={`language  ${darkMode ? "lwhite" : "lblack"} }`}
+                >
+                  {list.planguages[1]}
+                </div>
+                <div
+                  className={`language  ${darkMode ? "lwhite" : "lblack"} }`}
+                >
+                  {list.planguages[2]}
+                </div>
+              </div>
+              <h3
+                className={`pname  ${darkMode ? "pblue" : "pblack"} }`}
+                style={{ color: `${darkMode ? "pwhite" : "pblack"}` }}
+              >
+                {list.ptilte}
+              </h3>
+              <p className={`${darkMode ? "pwhite" : "pblack"} }`}>
+                {list.pdescription}
+              </p>
+
+              <a
+                href={list.plink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="project-link"
+              >
+                <button className="view_btn">View Project</button>
+              </a>
             </div>
           );
         })}
